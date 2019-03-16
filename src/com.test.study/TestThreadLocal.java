c9d2701e5b27f408d4f48eaed08215089a12421d
xujingrui3 <jingruiguyue@sina.com>
2019-03-16 18:36:37 +0800

    第二次测试

---
+++ src/com.test.study/TestThreadLocal.java
@@ -0,0 +1,10 @@
+package com.test.study;
+
+/**
+ * Created by xujingrui3 on 2019/3/16.
+ */
+public class TestThreadLocal {
+    public static void main(String[] args) {
+        System.out.println("打印并提交！！！");
+    }
+}
