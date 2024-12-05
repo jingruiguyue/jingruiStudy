4a8a492c3b3cfc8ab38f1cd7d0874b5c5ee3a03e
BRUCEXU\Bruce <xu.jingrui@supp.co.jp>
2024-12-05 17:45:10 +0900

    测试

---
+++ src/com/test/study/think/ThreeDThink.java
@@ -0,0 +1,17 @@
+package com.test.study.think;
+
+/**
+ * @Classname ThreeDThink
+ * @Description TODO
+ * @Date 2024/12/05 11:55
+ * @Created by Bruce
+ */
+public class ThreeDThink {
+//    1.三位数中定一位，其他两位定四位，共计32
+//    2.三位数中定两位，其他定6位或4位 共计12+8=20;
+//    3.如果十次对一次，投入是320收获1040盈利720
+//    4.重点还是选定两位，然后投8倍8320
+//    5.
+//    6.
+//    7.
+}
